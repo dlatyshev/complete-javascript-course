78c53594cd2323c132a8a26c85e9ce8dc7ca06ae
Dmytro Latyshev <d_latyshev@ukr.net>
2024-06-25 12:41:23 +0300

    Declaring variables

---
+++ 01-Fundamentals-Part-1/my-files/data-types.js
@@ -0,0 +1,15 @@
+// Number - used for decimals and integers
+// String - user for characters sets
+// Boolean - Logical type that can ony be true or false
+// Undefined - Value taken by a variable that is not yet defined
+// Null - Also means "emmpty value"
+// Symbol - Value that is unique and cannot be changed
+// BigInt - Larger integers then the Number type can hold
+
+// typeof - special operator to check type of some variable or literal
+
+console.log(typeof true);
+console.log(typeof (10));
+console.log(typeof (1.1));
+console.log(typeof undefined);
+console.log(typeof null); // this is a javascript bug. This expression returns 'object' as a type. But null is no an object.
--- 01-Fundamentals-Part-1/my-files/variables.js
+++ 01-Fundamentals-Part-1/my-files/variables.js
@@ -5,3 +5,14 @@ let lastName = "Latyshev";
 console.log(`Your name is ${firstName} ${lastName}!`)
 
 let age = 35;
+
+// Declaring variables with var, let and const
+let myLetVariable = "Hello"; // variable cab be changed later
+myLetVariable = 11;
+console.log(myLetVariable);
+
+const PI = 3.14; // Cannot be changed later.
+//PI = 3.15 //error!
+
+
+var name = "Oleg"; // old way to declare variables.
