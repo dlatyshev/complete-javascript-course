56dc5587b3cf4985bd366ea0276bfc53bc834494
Dmytro Latyshev <d_latyshev@ukr.net>
2024-06-25 13:23:05 +0300

    Basic Operators

---
+++ 01-Fundamentals-Part-1/my-files/basic-operators.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 20;
+
+const subtractionBandA = b - a; // 10
+const additionAandB = a + b; // 30
+const multiplicationAandB = a * b; // 200
+const divisionAandB = a / b; // 0.5
+
+const aGreaterThanB = a > b; // false
+const aLessThanB = a < b; // true
+const aNotEqualToB = a != b; // true
+
