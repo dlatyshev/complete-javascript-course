3df7f29c132fee4917723d75a82ef2af9a78d5d7
Dmytro Latyshev <d_latyshev@ukr.net>
2024-06-25 14:07:55 +0300

    String tempaltes

---
+++ 01-Fundamentals-Part-1/my-files/strings-and-templates.js
@@ -0,0 +1,17 @@
+const firstName = 'Dima';
+const job = 'AQA';
+const birthYear = 1989;
+const currentYear = 2024;
+const dima = "I'm " + firstName + ", an " + job + ", " + (currentYear - birthYear) + " years old."
+console.log(dima);
+const dimaNew = `I'm ${firstName}, an ${job}, ${currentYear - birthYear} years old.`;
+console.log(dimaNew);
+
+// multiline string
+
+let myString = `Line one
+Line two
+Line three
+`;
+
+console.log(myString);
