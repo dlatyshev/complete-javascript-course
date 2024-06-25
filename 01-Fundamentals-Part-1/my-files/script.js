6d2a41c575c3f45ac70e482d4ce1935700f49d4f
Dmytro Latyshev <d_latyshev@ukr.net>
2024-06-25 11:35:58 +0300

    Lesson 1

---
+++ 01-Fundamentals-Part-1/my-files/script.js
@@ -0,0 +1,3 @@
+let js = "Amazing";
+if (js == "Amazing") alert("JS is amazing!");
+console.log(1 + 2 + 3 + 4);
---
+++ 01-Fundamentals-Part-1/my-files/variables.js
@@ -0,0 +1,7 @@
+let firstName = "Dima";
+let lastName = "Latyshev";
+
+
+console.log(`Your name is ${firstName} ${lastName}!`)
+
+let age = 35;
