5ae7fc60ccc406835fcea1abe81a6e6928b12c5f
Dmytro Latyshev <d_latyshev@ukr.net>
2024-06-25 18:57:32 +0300

    Decisions

---
+++ 01-Fundamentals-Part-1/my-files/decisions.js
@@ -0,0 +1,10 @@
+const age = 18;
+
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+    console.log("Your are old enough!");
+} else {
+    console.log("You are not allowed to be here.");
+}
+
