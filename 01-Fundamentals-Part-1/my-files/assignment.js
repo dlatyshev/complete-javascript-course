7070804c28cb4120ed04673668d04e89fbe4c157
Dmytro Latyshev <d_latyshev@ukr.net>
2024-06-25 11:41:47 +0300

    Assignment for variables

---
+++ 01-Fundamentals-Part-1/my-files/assignment.js
@@ -0,0 +1,5 @@
+let country = "Ukraine";
+let continent = "Eurasia";
+let population = 33000000;
+
+console.log(`Your county is ${country} which located in ${continent} with a population of ${population} people.`)
