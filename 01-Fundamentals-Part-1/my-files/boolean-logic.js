de0a6692f44f3c2678bc41b867f961420dc0a334
Dmytro Latyshev <d_latyshev@ukr.net>
2024-06-27 10:34:19 +0300

    Boolean logic

---
+++ 01-Fundamentals-Part-1/my-files/boolean-logic.js
@@ -0,0 +1,13 @@
+const a = true;
+const b = false;
+
+console.log(a && b); // false
+console.log(a || b); // true
+console.log(!a); // false
+
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision); // true
+
