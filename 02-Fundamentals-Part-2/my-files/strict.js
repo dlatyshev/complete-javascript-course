5e06d1e42f3f395417f062b2f5c873754e6cfda8
Dmytro Latyshev <d_latyshev@ukr.net>
2024-06-28 12:55:25 +0300

    Strict mode

---
+++ 02-Fundamentals-Part-2/my-files/strict.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+
+if (passTest) {
+    hasDriverLicense = true; // will be an error
+}
+
+if (hasDriversLicense) {
+    console.log("I can driver.");
+}
+
+const interface = "hello"; // will be an error two.
+
+
