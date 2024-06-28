e0c92112c923cec485f4dcbad8d53434cbc185b5
dl-ct <dmytro.latyshev@customertimes.com>
2024-06-28 15:29:58 +0300

    Objects

---
+++ 02-Fundamentals-Part-2/my-files/objects.js
@@ -0,0 +1,13 @@
+const dima = {
+    name: "Dima",
+    surname: "Latyshev",
+    age: 35
+} // Object literal
+
+const log = console.log;
+
+log(dima);
+
+log(dima.name)
+log(dima['name'])
+log(dima['a' + 'g' + 'e']);
