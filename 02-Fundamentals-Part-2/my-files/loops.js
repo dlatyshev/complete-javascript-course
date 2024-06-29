9d90a1bb81838e08bdf0bc0f1bbe4c025b74d8a0
dl-ct <dmytro.latyshev@customertimes.com>
2024-06-29 22:01:12 +0300

    Loops

---
+++ 02-Fundamentals-Part-2/my-files/loops.js
@@ -0,0 +1,15 @@
+// comment
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let name of ["dima", "yura", "katya"]) {
+  console.log(name);
+}
+
+let x = 0;
+while (x != 10) {
+  console.log(x);
+  x++;
+}
