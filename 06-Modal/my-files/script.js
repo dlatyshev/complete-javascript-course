42a730cee6bac4887c61077098130c99c35acac2
dl-ct <dmytro.latyshev@customertimes.com>
2024-07-05 09:06:37 +0300

    Modal Manipulation

---
+++ 06-Modal/my-files/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const showModalBtns = document.querySelectorAll('.show-modal');
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let showModalButton of showModalBtns) {
+  showModalButton.addEventListener('click', openModal);
+}
+
+closeModalBtn.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key == 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
